38f8d5bf2450dafcb3538af9230b9ed933a11822
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-01-20 13:35:14 -0600

    created tests for chatacter

---
+++ __tests__/character.spec.ts
@@ -0,0 +1,33 @@
+import Character from '../src/logic/character'
+
+const James = new Character('man', false, true,'white','brown');
+const Jenny = new Character('woman', true, false, 'hispanic', 'black');
+
+
+describe('James', ()=>{
+    test('is a man',()=>{
+        expect(James.guessGender('man')).toBeTruthy();
+    })
+    test('does not have glasses', () => {
+        expect(James.guessGlasses(false)).toBeTruthy();
+    })
+    test('does have hat', () => {
+        expect(James.guessHat(true)).toBeTruthy();
+    })
+    test('is white', () => {
+        expect(James.guessRace('white')).toBeTruthy();
+    })
+    test('has brown hair', () => {
+        expect(James.guessHair('brown')).toBeTruthy();
+    })
+})
+
+describe('Jenny',()=>{
+    test('is not a man', ()=>{
+        expect(Jenny.guessGender('man')).toBeFalsy();
+    })
+    test('is a woman', ()=>{
+        expect(Jenny.guessGender('woman')).toBeTruthy();
+    })
+
+})
