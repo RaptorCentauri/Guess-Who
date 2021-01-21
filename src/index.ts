59a63259dc41611306a806c4760bc447e267ccea
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-01-21 12:53:39 -0600

    current playground for gameplay testing

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import Player from './logic/player'
+import roster from './logic/roster';
+
+
+const selectCharacterIDs = () => {
+    let randOne = Math.floor(Math.random() * roster.length-1);
+    let randTwo = Math.floor(Math.random() * roster.length - 1);
+
+    if(randOne === randTwo){
+        selectCharacterIDs();
+    }
+    else{
+        let randomNums = [];
+        randomNums.push(randOne)
+        randomNums.push(randTwo);
+        return randomNums;
+    }
+}
+
+let characterIDs = selectCharacterIDs()
+
+const playerOne = new Player(roster, characterIDs[0] );
+const playerTwo = new Player(roster, characterIDs[1]);
+
+
+let pOneChar = playerOne.sayMyCharacter();
+let pTwoChar = playerTwo.sayMyCharacter();
+
+
+console.log(pOneChar.sayGender());
+console.log(pTwoChar.sayGender());
+
+console.log('p1 current before', playerOne.sayCurrentCandidates());
+
+
+
+
+playerOne.guessOpponentGender('man', pTwoChar.sayGender())
+// playerOne.guessOpponentGender('woman', pTwoChar.sayGender())
+
+console.log('p1 current after', playerOne.sayCurrentCandidates());
+
+
+
+
+
+
