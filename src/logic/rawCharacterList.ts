9516b4b87ec86918370d59c101a0363fad57c71e
RaptorCentauri <dave.leo.shilander@icloud.com>
2021-01-21 12:52:18 -0600

    create list of potential cards

---
+++ src/logic/rawCharacterList.ts
@@ -0,0 +1,59 @@
+const rawCharacterList = [
+    {
+        id: 0,
+        name:'George',
+        gender:'man',
+        glasses:true,
+        hat:true,
+        race:'caucasian',
+        hair:'brown'    
+    },
+    {
+        id: 1,
+        name: 'Ralph',
+        gender: 'man',
+        glasses:false,
+        hat: true,
+        race: 'african',
+        hair: 'black'
+    },
+    {
+        id: 2,
+        name: 'Kevin',
+        gender: 'man',
+        glasses: false,
+        hat: false,
+        race: 'asian',
+        hair: 'red'
+    },
+    {
+        id: 3,
+        name: 'Jenny',
+        gender: 'woman',
+        glasses:true,
+        hat: false,
+        race: 'african',
+        hair: 'black'
+    },
+    {
+        id: 4,
+        name: 'April',
+        gender: 'woman',
+        glasses:false,
+        hat: false,
+        race: 'caucasian',
+        hair: 'blonde'
+    },
+    {
+        id: 5,
+        name: 'Laura',
+        gender: 'woman',
+        glasses: true,
+        hat: true,
+        race: 'asian',
+        hair: 'red'
+    },
+]
+
+
+export default rawCharacterList;
